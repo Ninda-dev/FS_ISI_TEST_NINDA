065ffeb99d58023b635a875dbe9c9597f89c757b
Ninda-dev <armilaninda@gmail.com>
2025-05-21 21:04:11 +0700

    CRUD frontend

--- frontend/src/components/TodoCard.jsx
+++ frontend/src/components/TodoCard.jsx
@@ -1,57 +1,157 @@
 import { Circle, CircleCheck, CircleX, Pencil } from "lucide-react";
 import PropTypes from "prop-types";
 import { formatDate } from "../utils/formatDate";
+import { useState } from "react";
 
+export default function TodoCard({ ongoingTasks, completedTasks, fetchOngoingTasks, fetchCompletedTasks, onEditTask}) {
+  const [hoveredId, setHoveredId] = useState(null);
+  console.log("ongoingTasks>>>>>>>>>", ongoingTasks);
 
-export default function TodoCard({tasks}) {
-  console.log("Tasks>>>>>", tasks);
+  const handleUpdate = (task) => {
+    console.log("Update task with ID:", task);
+
+    const fetchUpdateTask = async () => {
+      try {
+        const response = await fetch(`http://localhost:8000/tasks/${task.id}`, {
+          method: "PUT",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({
+            title: task.title,
+            id: task.id,
+            is_done: true,
+          }),
+        });
+
+        if (!response.ok) {
+          throw new Error("Network response was not ok");
+        }
+
+        alert("Task updated successfully");
+        fetchOngoingTasks();
+        fetchCompletedTasks();
+      } catch (error) {
+        console.error("Error updating task:", error);
+      }
+    };
+
+    fetchUpdateTask();
+  };
+
+  const handleDelete = (taskId) => {
+    // console.log("Delete task with ID:", task);
+
+    const fetchDeleteTask = async () => {
+      try {
+        if (!confirm("Are you sure you want to delete this task?")) {
+          return;
+        }
+
+        const response = await fetch(`http://localhost:8000/tasks/${taskId}`, {
+          method: "DELETE",
+        });
+
+        if (!response.ok) {
+          throw new Error("Network response was not ok");
+        }
+
+        alert("Task deleted successfully");
+        fetchOngoingTasks();
+        fetchCompletedTasks();
+      } catch (error) {
+        console.error("Error deleting task:", error);
+      }
+    };
+
+    fetchDeleteTask();
+  };
+
   return (
     <>
       <div>
+        {/* ongoing tasks */}
         <div className="sm:col-span-4 w-full mb-4 gap-4">
           <label className="block font-bold text-gray-900 my-4">Ongoing Task</label>
-          {tasks.map((task) => {
-            return (
-              <div className="flex justify-between items-center rounded-lg bg-gray-200 w-full px-4 py-4 my-2">
-                <div className="flex flex-col">
-                  <div className="flex flex-row items-baseline gap-2">
-                    <label htmlFor="" className="text-sm">
-                      {task.title}
-                    </label>
-                    <Pencil strokeWidth={3} className="w-3 h-3" />
-                  </div>
-                  <label htmlFor="" className="text-xs">
-                    {formatDate(task.created_at)}
-                  </label>
-                </div>
-                <div className="flex flex-row gap-2">
-                  <CircleX />
-                  <Circle fill="white" />
-                </div>
-              </div>
-            );
-          })}
-        </div>
-
-        <div>
-          <label className="block font-bold text-gray-900">Completed Task</label>
-          <div className="flex justify-between items-center rounded-lg bg-gray-200 w-full px-4 py-4">
-            <div className="flex flex-col">
-              <div className="flex flex-row items-baseline gap-2">
-                <label htmlFor="" className="text-sm line-through">
-                  Tugas 2
-                </label>
-                <Pencil strokeWidth={3} className="w-3 h-3" />
-              </div>
-              <label htmlFor="" className="text-xs">
-                25 April 2024 08:00
+          {ongoingTasks.length === 0 && (
+            <div className="flex justify-center items-center rounded-lg bg-gray-200 w-full px-4 py-4 my-2">
+              <label htmlFor="" className="text-sm">
+                No ongoing tasks
               </label>
             </div>
-            <div className="flex flex-row gap-2">
-              <CircleX />
-              <CircleCheck />
+          )}
+          {ongoingTasks.length > 0 && (
+            <>
+              {ongoingTasks.map((task) => {
+                return (
+                  <div className="flex justify-between items-center rounded-lg bg-gray-200 w-full px-4 py-4 my-2">
+                    <div className="flex flex-col">
+                      <div className="flex flex-row items-baseline gap-2">
+                        <label htmlFor="" className="text-sm">
+                          {task.title}
+                        </label>
+                        <Pencil strokeWidth={3} className="w-3 h-3" onClick={() => onEditTask(task)}/>
+                      </div>
+                      <label htmlFor="" className="text-xs">
+                        {formatDate(task.created_at)}
+                      </label>
+                    </div>
+                    <div className="flex flex-row gap-2">
+                      <CircleX
+                        onClick={() => {
+                          handleDelete(task.id);
+                        }}
+                      />
+                      <span onMouseEnter={() => setHoveredId(task.id)} onMouseLeave={() => setHoveredId(null)}>
+                        {hoveredId === task.id ? <CircleCheck fill="white" className="text-green-500" onClick={() => handleUpdate(task)} /> : <Circle fill="white" />}
+                      </span>
+                    </div>
+                  </div>
+                );
+              })}
+            </>
+          )}
+        </div>
+
+        {/* completed tasks */}
+        <div>
+          <label className="block font-bold text-gray-900">Completed Task</label>
+          {completedTasks.length === 0 && (
+            <div className="flex justify-center items-center rounded-lg bg-gray-200 w-full px-4 py-4 my-2">
+              <label htmlFor="" className="text-sm">
+                No completed tasks
+              </label>
             </div>
-          </div>
+          )}
+          {completedTasks.length > 0 && (
+            <>
+              {completedTasks.map((task) => {
+                return (
+                  <div className="flex justify-between items-center rounded-lg bg-gray-200 w-full px-4 py-4 my-2">
+                    <div className="flex flex-col">
+                      <div className="flex flex-row items-baseline gap-2">
+                        <label htmlFor="" className="text-sm">
+                          {task.title}
+                        </label>
+                        <Pencil strokeWidth={3} className="w-3 h-3" onClick={() => onEditTask(task)}/>
+                      </div>
+                      <label htmlFor="" className="text-xs">
+                        {formatDate(task.created_at)}
+                      </label>
+                    </div>
+                    <div className="flex flex-row gap-2">
+                      <CircleX
+                        onClick={() => {
+                          handleDelete(task.id);
+                        }}
+                      />
+                      <CircleCheck />
+                    </div>
+                  </div>
+                );
+              })}
+            </>
+          )}
         </div>
       </div>
     </>
@@ -59,5 +159,9 @@ export default function TodoCard({tasks}) {
 }
 
 TodoCard.PropTypes = {
-  tasks: PropTypes.array.isRequired,
+  ongoingTasks: PropTypes.array.isRequired,
+  completedTasks: PropTypes.array.isRequired,
+  fetchOngoingTasks: PropTypes.func.isRequired,
+  fetchCompletedTasks: PropTypes.func.isRequired,
+  onEditTask: PropTypes.func.isRequired,
 };
--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -2,52 +2,151 @@ import { use, useEffect, useState } from "react";
 import TodoCard from "../components/TodoCard";
 
 export default function Home() {
+  const [ongoingTasks, setOngoingTasks] = useState([]);
+  const [completedTasks, setCompletedTasks] = useState([]);
+  const [editTask, setEditTask] = useState(null);
   const [isUpdate, setIsUpdate] = useState(false);
-  const [tasks, setTasks] = useState([]);
+  const [title, setTitle] = useState("");
 
   useEffect(() => {
-    const fetchTasks = async () => {
+    fetchOngoingTasks();
+  }, []);
+
+  useEffect(() => {
+    fetchCompletedTasks();
+  }, []);
+
+  const fetchOngoingTasks = async () => {
+    try {
+      const response = await fetch("http://localhost:8000/tasks?is_done=false");
+      const data = await response.json();
+
+      setOngoingTasks(data);
+    } catch (error) {
+      console.error("Error fetching tasks:", error);
+    }
+  };
+
+  const fetchCompletedTasks = async () => {
+    try {
+      const response = await fetch("http://localhost:8000/tasks?is_done=true");
+      const data = await response.json();
+
+      setCompletedTasks(data);
+    } catch (error) {
+      console.error("Error fetching tasks:", error);
+    }
+  };
+
+  const handleEditTask = (task) => {
+    setEditTask(task);
+    setIsUpdate(true);
+    setTitle(task.title);
+  }
+
+  const handleUpdateTask = async (e) => {
+    e.preventDefault();
+    const formData = new FormData(e.target);
+    const title = formData.get("title");
+    console.log("Update task with ID:");
+
+    const fetchUpdateTask = async () => {
       try {
-        const response = await fetch("http://localhost:8000/tasks?is_done=false");
-        const data = await response.json();
-        // console.log("Data>>>>>", data);
-        setTasks(data);
+        const response = await fetch(`http://localhost:8000/tasks/${editTask.id}`, {
+          method: "PUT",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({
+            title: title,
+            id: editTask.id,
+            is_done: editTask.is_done,
+          }),
+        });
+
+        if (!response.ok) {
+          throw new Error("Network response was not ok");
+        }
+
+        alert("Task updated successfully");
+        e.target.reset();
+        setIsUpdate(false);
+        setEditTask(null);
+        fetchOngoingTasks();
+        fetchCompletedTasks();
       } catch (error) {
-        console.error("Error fetching tasks:", error);
+        console.error("Error updating task:", error);
       }
     };
 
-    fetchTasks();
-  }, []);
+    fetchUpdateTask();
+  }
 
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    console.log("e >>>>>>>>>>>>>>", e);
+    const formData = new FormData(e.target);
+    const title = formData.get("title");
+
+    try {
+      const response = await fetch("http://localhost:8000/tasks", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          title,
+          is_done: false,
+        }),
+      });
+
+      if (!response.ok) {
+        throw new Error("Network response was not ok");
+      }
+
+      alert("Task added successfully");
+      setTitle("");
+      fetchCompletedTasks();
+      fetchOngoingTasks();
+    } catch (error) {
+      console.error("Error adding task:", error);
+    }
+  };
+
+
+  console.log("isUpdate >>>>>>>>>>>>>>", isUpdate); 
   return (
     <>
       <h1 className="text-3xl font-medium my-4" style={{ textAlign: "center" }}>
         Task Management
       </h1>
       <div className="w-1/3 mx-auto min-h-screen flex flex-col justify-items-center align-items-start">
-        <form action="" className="w-full flex flex-row items-center">
+        <form onSubmit={isUpdate ? handleUpdateTask : handleSubmit} className="w-full flex flex-row items-center">
           <div className="sm:col-span-4 w-full mb-4 gap-4 items-center">
             <label for="title" className="block text-sm text-gray-900">
               Title
             </label>
             <div className="mb-4">
               <div className="flex items-center rounded-md bg-white pl-3 outline-1 -outline-offset-1 outline-gray-500 focus-within:outline-2 focus-within:-outline-offset-2 focus-within:outline-gray-600">
-                <input type="text" name="title" id="title" className="block w-full min-w-0 grow py-1.5 pr-3 pl-1 text-base text-gray-900 placeholder:text-gray-400 focus:outline-none sm:text-sm/6" />
+                <input type="text" name="title" id="title" className="block w-full min-w-0 grow py-1.5 pr-3 pl-1 text-base text-gray-900 placeholder:text-gray-400 focus:outline-none sm:text-sm/6" 
+                defaultValue={isUpdate && editTask ? editTask.title : ""}
+                value={title}
+                onChange={(e) => setTitle(e.target.value)}
+                />
               </div>
             </div>
             <div className="sm:col-span-4 w-full mb-4 flex justify-center">
               {isUpdate ? (
                 <>
-                  <button type="button" className="rounded-xl bg-orange-300 hover:text-white items-center px-2 py-1 mr-2" onClick={() => setIsUpdate(false)}>
+                  <button type="submit" className="rounded-xl bg-orange-300 hover:text-white items-center px-2 py-1 mr-2" onClick={() => setIsUpdate(true)}>
                     Update Task
                   </button>
-                  <button type="button" className="rounded-xl bg-red-400 hover:text-white items-center px-4 py-1" onClick={() => setIsUpdate(false)}>
+                  <button type="button" className="rounded-xl bg-red-400 hover:text-white items-center px-4 py-1" onClick={() => {setEditTask(null); setIsUpdate(false); setTitle("");}}>
                     Cancel
                   </button>
                 </>
               ) : (
-                <button type="button" className="rounded-xl bg-sky-blue hover:text-white items-center px-4 py-1">
+                <button type="submit" className="btn rounded-xl bg-sky-blue hover:text-white items-center px-4 py-1">
                   Add Task
                 </button>
               )}
@@ -55,7 +154,7 @@ export default function Home() {
           </div>
         </form>
 
-        <TodoCard tasks={tasks}/>
+        <TodoCard ongoingTasks={ongoingTasks} completedTasks={completedTasks} fetchOngoingTasks={fetchOngoingTasks} fetchCompletedTasks={fetchCompletedTasks} onEditTask={handleEditTask}/>
       </div>
     </>
   );
